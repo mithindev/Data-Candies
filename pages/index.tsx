ff415df6dbf332b882ed525ab9f4cf37e1e3d04f
mithindev <mithindev1@gmail.com>
2022-12-26 16:44:48 +0530

    updated

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+
+    </div>
+  )
+}
+
